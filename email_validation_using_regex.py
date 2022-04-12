844f72e9e174862693fd40153cd3f1663a1a07db
Nitika Sachdeva <98147893+NitikaSachdeva9@users.noreply.github.com>
2022-04-12 17:14:36 +0530

    Update email_validation_using_regex.py

--- email_validation_using_regex.py
+++ email_validation_using_regex.py
@@ -1,4 +1,10 @@
 #This program validates the email address provided by the user by using regex to check the conditions.
+#Conditions - 
+#1. Check all the chars are lower case
+#2. Check all the chars are a-z, 0-9
+#3. '.' and '_' can occur once in the string
+#4. '@' can occur once in the string
+#5. 2,3 char from the back should be a '.'
 import re
 email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
 user_email=input('Enter your Email Address: ')
